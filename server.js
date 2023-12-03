3d277c0cd855ba4a5fa413aed669fc185f6389d8
aalyaanna <75129917+aalyaanna@users.noreply.github.com>
2023-12-03 20:19:30 +0800

    modified:   public/navbar.css 	modified:   server.js 	modified:   views/header.ejs 	modified:   views/homepage.ejs

--- server.js
+++ server.js
@@ -9,32 +9,35 @@ const nodemailer = require('nodemailer')
 const uuid = require('uuid');
 const multer = require('multer');
 
-// const { PrismaClient } = require('@prisma/client');
-// const prisma = new PrismaClient();
-// const DATABASE_URL = process.env.DATABASE_URL;
+const {
+    PrismaClient
+} = require('@prisma/client');
+const prisma = new PrismaClient();
+const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 //database connection
-// async function connectToDatabase() {
-//     try {
-//       await prisma.$connect();
-//       console.log('Connected to the database');
-//     } catch (error) {
-//       console.error('Error connecting to the database:', error);
-//     }
-//   }
+async function connectToDatabase() {
+    try {
+        await prisma.$connect();
+        console.log('Connected to the database');
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
+}
 
-//   connectToDatabase();
+connectToDatabase();
 
-//transporter for gmail authentication link, change accessToken after expriration 
+//transporter for gmail authentication link
 const transport = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         type: 'OAuth2',
         user: 'soundsendofficial@gmail.com',
-        accessToken: 'ya29.a0AfB_byBodpxcG6yMTr7dV1Iex0Ur6wl9lhu2sqzdSxHUJ1ERa3TeOJKbuBPNGaWvLkoHpihdJBYEwIY2H1ZgWT43jBLsPY7tey21hmUPCivm8_fw3cckVXDU-3rOPMXblwMd1zChIqy6WPl5aClR7x7xcnLYjEiCQrJCaCgYKASUSARESFQGOcNnClejYgPyGm_sEYbtlTtC0eA0171'
+        accessToken: 'ya29.a0AfB_byCheS5EUZyQMIIOy-X8OUbr6oqXWOh7GG3pmIr1U3WTn9OSpSUMVrrOq73PCe63AQCZnfcRWiioA4YeoiegZQ9O_lwFuPQ6NpCKBTr9G9b4-aeDKZnxKXnWZaGXOKaJEeVCBezAaP_AIi_weciovgJ4h7HUxsGQaCgYKAWcSARISFQHGX2MiM6PGOxfZoTfBoXfSioo34A0171'
     }
 })
 
@@ -42,8 +45,8 @@ const transport = nodemailer.createTransport({
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'oshisoffline@gmail.com',
-        pass: 'txxdvicihtvcgevy'
+        user: 'soundsendofficial@gmail.com',
+        pass: 'cqhhguzvcslpdeop'
     }
 })
 
@@ -75,7 +78,7 @@ app.use('/', router);
 
 app.get('/', (req, res) => {
     res.render('welcomepage', {
-        title: "SoundSend"
+        title: "EyeDaptify"
     });
 })
 
@@ -86,7 +89,7 @@ app.post('/', (req, res) => {
     const queriesOptions = {
         from: req.body.address,
         to: 'soundsendofficial@gmail.com',
-        subject: `Feedback from ${req.body.name} at SoundSend Website`,
+        subject: `Feedback from ${req.body.name} at EyeDaptify Website`,
         text: `The feedback from ${req.body.address}\nSubject: ${req.body.subject}\nQueries: ${req.body.queries}`
     }
 
@@ -122,10 +125,10 @@ app.post('/login', async (req, res) => {
             to: email,
             subject: 'Magic Auth Link',
             html: `
-        <p>Click link below to access SoundSend Official Web Page.<p>
-        <a href="http://localhost:3000/homepage?email=${encodeURIComponent(
+        <p>Click link below to access EyeDaptify Official Web Page.<p>
+        <a href="https://eyedaptify.onrender.com/homepage?email=${encodeURIComponent(
             email
-        )}&code=${encodeURIComponent(magicCode)}">SoundSend Official</a>
+        )}&code=${encodeURIComponent(magicCode)}">EyeDaptify Official</a>
         `,
         };
 
@@ -157,7 +160,7 @@ app.get('/homepage', async (req, res) => {
     res.redirect('/')
 });
 
-//sending emails, to be edit, gagawing dynamic 'yong userEmailAddress
+//sending emails, using Nodemailer
 app.post('/send-email', async (req, res) => {
 
     upload(req, res, async function (err) {
@@ -190,7 +193,7 @@ app.post('/send-email', async (req, res) => {
                 console.error(error);
                 res.status(500).json({
                     status: 'error',
-                    message: 'Error sending email'
+                    message: 'Error sending email...'
                 });
             } else {
                 console.log('Email sent: ' + info.response);

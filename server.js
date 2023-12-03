fa9be8d5003a53d0a83964593735b5ca69c0cc6e
Liya-Aceron Oseo <133098907+poetry4vamp@users.noreply.github.com>
2023-12-03 18:36:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,38 +3,39 @@ const bodyparser = require('body-parser');
 const session = require('express-session');
 
 const path = require('path');
-const router = require('./router');
+const router = require ('./router');
 
-const nodemailer = require('nodemailer')
-const uuid = require('uuid');
+const nodemailer = require ('nodemailer')
+const uuid = require ('uuid');
 const multer = require('multer');
 
-// const { PrismaClient } = require('@prisma/client');
-// const prisma = new PrismaClient();
-// const DATABASE_URL = process.env.DATABASE_URL;
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 //database connection
-// async function connectToDatabase() {
-//     try {
-//       await prisma.$connect();
-//       console.log('Connected to the database');
-//     } catch (error) {
-//       console.error('Error connecting to the database:', error);
-//     }
-//   }
+async function connectToDatabase() {
+    try {
+      await prisma.$connect();
+      console.log('Connected to the database');
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+    }
+  }
+  
+  connectToDatabase();
 
-//   connectToDatabase();
-
-//transporter for gmail authentication link, change accessToken after expriration 
+//transporter for gmail authentication link
 const transport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        type: 'OAuth2',
+    service:'gmail',
+    auth:{
+        type:'OAuth2',
         user: 'soundsendofficial@gmail.com',
-        accessToken: 'ya29.a0AfB_byBodpxcG6yMTr7dV1Iex0Ur6wl9lhu2sqzdSxHUJ1ERa3TeOJKbuBPNGaWvLkoHpihdJBYEwIY2H1ZgWT43jBLsPY7tey21hmUPCivm8_fw3cckVXDU-3rOPMXblwMd1zChIqy6WPl5aClR7x7xcnLYjEiCQrJCaCgYKASUSARESFQGOcNnClejYgPyGm_sEYbtlTtC0eA0171'
+        accessToken: 'ya29.a0AfB_byCheS5EUZyQMIIOy-X8OUbr6oqXWOh7GG3pmIr1U3WTn9OSpSUMVrrOq73PCe63AQCZnfcRWiioA4YeoiegZQ9O_lwFuPQ6NpCKBTr9G9b4-aeDKZnxKXnWZaGXOKaJEeVCBezAaP_AIi_weciovgJ4h7HUxsGQaCgYKAWcSARISFQHGX2MiM6PGOxfZoTfBoXfSioo34A0171'
     }
 })
 
@@ -42,29 +43,27 @@ const transport = nodemailer.createTransport({
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'oshisoffline@gmail.com',
-        pass: 'txxdvicihtvcgevy'
+        user:'soundsend@gmail.com',
+        pass:'cqhhguzvcslpdeop'
     }
 })
 
 //declare the file path and root directory folder for attaching files
 const Storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, './uploads');
+    destination:function(req,file,callback){
+        callback(null,'./uploads');
     },
-    filename: function (req, file, callback) {
-        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    filename:function(req,file,callback){
+        callback(null,file.fieldname + "_" + Date.now() + "_" + file.originalname);
     }
 })
 
 const upload = multer({
-    storage: Storage
+    storage:Storage
 }).single('attachment');
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({
-    extended: true
-}))
+app.use(bodyparser.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs');
 
@@ -73,10 +72,8 @@ app.use(express.static(path.join(__dirname, 'public/scripts')))
 
 app.use('/', router);
 
-app.get('/', (req, res) => {
-    res.render('welcomepage', {
-        title: "SoundSend"
-    });
+app.get('/', (req, res) =>{
+    res.render('welcomepage', { title : "EyeDaptify"});
 })
 
 //function for sending queries
@@ -86,7 +83,7 @@ app.post('/', (req, res) => {
     const queriesOptions = {
         from: req.body.address,
         to: 'soundsendofficial@gmail.com',
-        subject: `Feedback from ${req.body.name} at SoundSend Website`,
+        subject: `Feedback from ${req.body.name} at EyeDaptify Website`,
         text: `The feedback from ${req.body.address}\nSubject: ${req.body.subject}\nQueries: ${req.body.queries}`
     }
 
@@ -102,54 +99,45 @@ app.post('/', (req, res) => {
 });
 
 //passwordless authentication link
-app.post('/login', async (req, res) => {
-    const {
-        email
-    } = req.body;
+app.post('/login',async (req,res) => {
+    const {email} = req.body;
 
     try {
         const magicCode = uuid.v4().substr(0, 8);
-
+    
         await prisma.emails.create({
-            data: {
-                email,
-                magicCode,
-            }
-        });
+          data: {
+            email,
+            magicCode,
+          }
+    });
 
-        const mailOptions = {
-            from: 'soundsendofficial@gmail.com',
-            to: email,
-            subject: 'Magic Auth Link',
-            html: `
-        <p>Click link below to access SoundSend Official Web Page.<p>
+    const mailOptions = {
+        from:'soundsendofficial@gmail.com',
+        to:email,
+        subject:'Magic Auth Link',
+        html: `
+        <p>Click link below to access EyeDaptify Official Web Page.<p>
         <a href="http://localhost:3000/homepage?email=${encodeURIComponent(
             email
-        )}&code=${encodeURIComponent(magicCode)}">SoundSend Official</a>
+        )}&code=${encodeURIComponent(magicCode)}">EyeDaptify Official</a>
         `,
-        };
+    };
 
         await transport.sendMail(mailOptions);
-        res.status(200).json({
-            message: "Magic Auth Link has been sent to your Gmail."
-        });
+        res.status(200).json({ message: "Magic Auth Link has been sent to your Gmail." });
     } catch (error) {
         console.error(error);
-        res.status(500).json({
-            message: "Error sending email..."
-        });
+        res.status(500).json({ message: "Error sending email..." });
     }
 })
 
 //the purpose of this code is to make sure isang beses lang available or pwede ma-access 'yong link
-app.get('/homepage', async (req, res) => {
-    const {
-        email,
-        code
-    } = req.query
+app.get('/homepage', async(req,res) => {
+    const {email,code} = req.query
     const user = users.find((u) => u.email === email && u.magicCode === code);
 
-    if (!user) {
+    if(!user){
         res.send("Invalid link!")
     }
 
@@ -157,57 +145,44 @@ app.get('/homepage', async (req, res) => {
     res.redirect('/')
 });
 
-//sending emails, to be edit, gagawing dynamic 'yong userEmailAddress
+//sending emails, using Nodemailer
 app.post('/send-email', async (req, res) => {
 
-    upload(req, res, async function (err) {
-        if (err) {
+    upload(req,res,async function(err){
+        if(err){
             console.log(err);
             return res.end("Something went wrong!");
         }
 
-        const attachment = req.file ? [{
-            path: req.file.path
-        }] : [];
+    const attachment = req.file ? [{
+        path: req.file.path
+    }] : [];
 
-        const {
-            userEmailAddress,
-            recipientEmailAddress,
-            subjectEmail,
-            bodyEmail
-        } = req.body;
+    const { userEmailAddress, recipientEmailAddress, subjectEmail, bodyEmail } = req.body;
 
-        const mailOptions = {
-            from: userEmailAddress,
-            to: recipientEmailAddress,
-            subject: subjectEmail,
-            text: bodyEmail,
-            attachments: attachment,
-        };
+    const mailOptions = {
+        from: userEmailAddress,
+        to: recipientEmailAddress,
+        subject: subjectEmail,
+        text: bodyEmail,
+        attachments: attachment,
+    };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error(error);
-                res.status(500).json({
-                    status: 'error',
-                    message: 'Error sending email'
-                });
-            } else {
-                console.log('Email sent: ' + info.response);
-                req.body.userEmailAddress = '';
-                req.body.recipientEmailAddress = '';
-                req.body.subjectEmail = '';
-                req.body.bodyEmail = '';
-                req.file = null;
-                res.status(200).json({
-                    status: 'success',
-                    message: 'Email sent successfully!'
-                });
-            }
-        });
-    })
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+        console.error(error);
+        res.status(500).json({ status: 'error', message: 'Error sending email...' });
+        } else {
+        console.log('Email sent: ' + info.response);
+            req.body.userEmailAddress = '';
+            req.body.recipientEmailAddress = '';
+            req.body.subjectEmail = '';
+            req.body.bodyEmail = '';
+            req.file = null;
+        res.status(200).json({ status: 'success', message: 'Email sent successfully!' });
+        }
+    });
+})
 })
 
-app.listen(port, () => {
-    console.log("Listening to the server on http://localhost:3000")
-});
+app.listen(port, ()=>{ console.log("Listening to the server on http://localhost:3000")});

3e3f5cdcba5587b0664d0ab0ae20be4101cbaa53
Liya-Aceron Oseo <133098907+poetry4vamp@users.noreply.github.com>
2023-12-04 17:20:42 +0800

    Update authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -2,16 +2,16 @@ const authenticatedUsers = [];
 
 const isAuthenticated = (req, res, next) => {
   const { email, code } = req.query;
-  console.log('Email:', email, 'Code:', code);
+  console.log('Email:', decodeURIComponent(email), 'Code:', decodeURIComponent(code));
 
   if (email && code) {
-      authenticatedUsers.push({ email, code });
-
+      //here, perform a check against the database if the provided email and code exist and are valid
+      //if they are valid, mark the user as authenticated
       req.isAuthenticated = true;
       req.user = { email, code };
       next();
   } else {
-      return res.send("Invalid link!");
+      return res.send('Invalid link!');
   }
 };
 

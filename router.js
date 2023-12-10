19a932b4b7c9024adc7b88c09e83c9eb0d5831d4
Liya-Aceron Oseo <133098907+poetry4vamp@users.noreply.github.com>
2023-12-10 22:07:18 +0800

    Update router.js

--- router.js
+++ router.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { isAuthenticated } = require('./authMiddleware');
+const { isAuthenticated, authenticatedUsers } = require('./authMiddleware');
 
 const renderInvalidLink = (res) => {
     res.send("Invalid link!");
 };
 
-router.get('/homepage', isAuthenticated, (req, res) => {
-    if (req.isAuthenticated) {
-        res.render('homepage');
-    } else {
-        renderInvalidLink(res);
-    }
-});
+// router.get('/homepage', isAuthenticated, (req, res) => {
+//     if (req.isAuthenticated) {
+//         res.render('homepage');
+//     } else {
+//         renderInvalidLink(res);
+//     }
+// });
+
+// router.get('/email', isAuthenticated, (req, res) => {
+//     if (req.isAuthenticated) {
+//         res.render('email');
+//     } else {
+//         renderInvalidLink(res);
+//     }
+// });
 
 router.get('/welcomepage', (req, res) => {
     res.render('welcomepage');
@@ -22,13 +30,9 @@ router.get('/email', (req, res) => {
     res.render('email');
 });
 
-// router.get('/email', isAuthenticated, (req, res) => {
-//     if (req.isAuthenticated) {
-//         res.render('email');
-//     } else {
-//         renderInvalidLink(res);
-//     }
-// });
+router.get('/homepage', (req, res) => {
+    res.render('homepage');
+});
 
 module.exports = {
     router,
